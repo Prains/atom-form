8bbf0f206450b3f49b491808a93135219e1564da
prains <sleskov2013@yandex.ru>
2024-08-09 21:07:50 +0300

    disable caching

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,5 +1,7 @@
 import { SuccessPage } from "@/components/views/success-page";
 
+export const revalidate = 0;
+
 const Page = () => <SuccessPage />;
 
 export default Page;
--- app/you-shall-not-pass-here/page.tsx
+++ app/you-shall-not-pass-here/page.tsx
@@ -1,6 +1,8 @@
 import { getSubmissions } from "@/lib/server-actions";
 import { ExcelPage } from "@/components/views/excel-page";
 
+export const revalidate = 0;
+
 const Page = async () => {
   const data = await getSubmissions();
 
